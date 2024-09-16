1cd314a75fbfef836d1a96f048a0d30b8704e9f6
HwangDat2004 <hoangtiendat.htd2004@gmail.com>
2024-09-16 18:01:35 +0700

    up

---
+++ src/app/pages/deatail/deatail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deatail',
+  standalone: true,
+  imports: [],
+  templateUrl: './deatail.component.html',
+  styleUrl: './deatail.component.css'
+})
+export class DeatailComponent {
+
+}
